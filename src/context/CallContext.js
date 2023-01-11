7c6a1e48ed22f9e44db502740d04ec9b0f6f2334
Yevhen <sluisarchuk@gmail.com>
2023-01-11 03:10:05 +0200

    port

--- src/context/CallContext.js
+++ src/context/CallContext.js
@@ -4,7 +4,7 @@ import { QueueContext } from './QueueContext';
 
 const CallContext = createContext();
 
-const socket = io(`${process.env.REACT_APP_API_ENDPOINT}:4000`);
+const socket = io(process.env.REACT_APP_API_ENDPOINT);
 
 const CallContextProvider = ({ children }) => {
   const { abortSpeakerSearch } = useContext(QueueContext);
