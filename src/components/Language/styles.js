8499d499d0ebfd24910668f81c1c055d7948924f
Yevhen <sluisarchuk@gmail.com>
2023-02-08 21:42:20 +0200

    ga

--- src/components/Language/styles.js
+++ src/components/Language/styles.js
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 
+import { device } from '../../constants';
+
 export const LanguageText = styled.a`
   color: ${(props) => props.theme.white};
   position: relative;
   left: 155px;
   top: -27px;
   text-transform: uppercase;
+
+  @media ${device.tablet} {
+    left: 10px;
+  }
 `;
