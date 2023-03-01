757375b5382e904c719882ae8fb2fb0312c1ff97
Yevhen <sluisarchuk@gmail.com>
2023-03-01 19:40:30 +0200

    hydration fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
-import { withTranslation } from 'react-i18next';
+import { Translation } from 'react-i18next';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -33,25 +33,13 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link
-            rel="preload"
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
-            as="style"
-            onLoad="this.onload=null;this.rel='stylesheet'"
+            rel="stylesheet"
           />
-          <noscript>
-            <link
-              href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
-              rel="stylesheet"
-              type="text/css"
-            />
-          </noscript>
           <meta name="theme-color" content="#000000" />
-          <meta
-            name="description"
-            content={this.props.t('description')}
-          />
+          <Translation>{(t) => <meta name="description" content={t('description')} />}</Translation>
         </Head>
         <body>
           <Main />
@@ -62,4 +50,4 @@ class MyDocument extends Document {
   }
 }
 
-export default withTranslation()(MyDocument);
+export default MyDocument;
